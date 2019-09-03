78cc505f12cd39a3cb36f877bfe44f08671d239e
benji <benjisp018@gmail.com>
2019-09-03 00:38:52 -0400

    adds restful routes to movie app

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,22 +1,40 @@
 class Api::MoviesController < ApplicationController
-  def movie_action
-    @movie = Movie.find_by(id: 1)
-    render 'movie.json.jb'
-  end
-
-  def show_all_movies
+  
+  def index
     @movie = Movie.all
     render 'movie.json.jb'
   end
 
-  def last_2_movies
-    @movie = Movie.last(2)
+  def show
+    @movie = Movie.find(params[:id])
     render 'movie.json.jb'
   end
 
-  def one_movie
-    input = params[:title]
-    @movie = Movie.find_by(title: input)
+  def create
+    @movie = Movie.new(
+    title: params[:title],
+    year: params[:year].to_i,
+    plot: params[:plot])
+    render 'movie.json.jb'
+    @movie.save
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.title = params[:title] || @movie.title
+    @movie.year = params[:year] || @movie.year
+    @movie.plot = params[:plot] || @movie.plot
+    @movie.save
+    render 'movie.json.jb'
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+  end
+
+  def last_2_movies
+    @movie = Movie.last(2)
     render 'movie.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/actors" => "actors#actor_action"
-    get "/movie" => "movies#movie_action"
-    get "/all_movies" => "movies#show_all_movies"
+    get "/movies" => "movies#index"
+    get "/movies/:id" => "movies#show"
+    post "/movies" => "movies#create"
+    patch "/movies/:id" => "movies#update"
+    delete "movies/:id" => "movies#destroy"
     get "/last_2" => "movies#last_2_movies"
-    get "one_movie" => "movies#one_movie"
-    get "segment_movie/:title" => "movies#one_movie"
-    post "body_movie" => "movies#one_movie"
   end
 end
