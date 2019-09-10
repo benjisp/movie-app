7f05be6528d764e5e92e5e54553aecb2ecabfc48
benji <benjisp018@gmail.com>
2019-09-10 03:19:51 -0400

    adds associations to movie app

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,24 +1,26 @@
 class Api::ActorsController < ApplicationController
-  
   def index
-    @actor = Actor.all.order(age: :desc)
-    render 'actor.json.jb'
+    @actors = Actor.all
+    render 'index.json.jb'
   end
 
   def show
     @actor = Actor.find(params[:id])
-    render 'actor.json.jb'
+    render 'show.json.jb'
   end
 
   def create
     @actor = Actor.new(
-    first_name: params[:first_name],
-    last_name: params[:last_name],
-    known_for: params[:known_for],
-    gender: params[:gender],
-    age: params[:age])
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+      gender: params[:gender],
+      age: params[:age],
+      movie_id: params[:movie_id]
+    )
+
     if @actor.save
-      render 'actor.json.jb'
+      render 'show.json.jb'
     else
       render json: {errors: @actor.errors.full_messages}, status: :unprocessable_entity
     end
@@ -26,15 +28,24 @@ class Api::ActorsController < ApplicationController
 
   def update
     @actor = Actor.find(params[:id])
+    
     @actor.first_name = params[:first_name] || @actor.first_name
     @actor.last_name = params[:last_name] || @actor.last_name
     @actor.known_for = params[:known_for] || @actor.known_for
     @actor.gender = params[:gender] || @actor.gender
     @actor.age = params[:age] || @actor.age
+    @actor.movie_id = params[:movie_id] || @actor.movie_id
+
     if @actor.save
-      render 'actor.json.jb'
+      render 'show.json.jb'
     else
       render json: {errors: @actor.errors.full_messages}, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @actor = Actor.find(params[:id])
+    @actor.destroy
+    render json: {message: "Actor successfully destroyed"}
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ApplicationRecord
 
-  validates :title, :year, presence: true
-  validates :title, uniqueness: true
-  validates :plot, length: { minimum: 10}
+  # validates :title, :year, presence: true
+  # validates :title, uniqueness: true
+  # validates :plot, length: { minimum: 10}
+
+  has_many :actors
 
 end
